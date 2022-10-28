14dc11fe8a3167b10cc57e5bde7cce7782d61073
Mark Farnsworth <farnsworth2008@gmail.com>
2022-10-28 18:36:54 +0000

    0.1.24-s3d-1026 Changed
    - Added `s3d-flow-json` in `changes` module

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ locals {
 }
 
 module "name" {
-  source = "github.com/s3d-club/terraform-external-name?ref=0.1.10-s3d-1015"
+  source = "github.com/s3d-club/terraform-external-name?ref=0.1.10-s3d-1016"
 
   context      = join("-", [var.name_prefix, var.template, var.setup_ref])
   disable_date = true
@@ -49,7 +49,7 @@ module "name" {
 }
 
 module "sg_egress" {
-  source = "github.com/s3d-club/terraform-aws-sg_egress_open?ref=0.1.11-s3d-1012"
+  source = "github.com/s3d-club/terraform-aws-sg_egress_open?ref=0.1.11-s3d-1013"
 
   cidr        = var.egress_cidrs
   cidr6       = var.egress_cidr6s
