b12e2899bbdc48018155aea660b966ec52afdb97
Mark Farnsworth <farnsworth2008@gmail.com>
2022-10-28 16:06:47 +0000

    0.1.24-s3d-1001 Changed
    - Added `s3d-flow-json` in `changes` module

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ module "sg_egress" {
 }
 
 module "sg_ingress" {
-  source = "github.com/s3d-club/terraform-aws-sg_ingress_ssh?ref=v0.1.9"
+  source = "github.com/s3d-club/terraform-aws-sg_ingress_ssh?ref=0.1.10-1000"
 
   cidr        = var.ssh_cidrs
   cidr6       = var.ssh_cidr6s
